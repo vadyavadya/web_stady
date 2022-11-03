fea633a3742beafdee14ffc081edb859bc2dcd6d
Vadim Sergeev <Vadim.Sergeev@evraz.com>
2022-11-03 15:56:52 +0700

    start job tfitness trainer

---
+++ fitness-traiiner/src/js/files/forms/datepicker.js
@@ -0,0 +1,28 @@
+/* Календарь */
+
+// Подключение функционала "Чертогов Фрилансера"
+// Подключение списка активных модулей
+import { flsModules } from "../modules.js";
+
+// Подключение модуля
+import datepicker from 'js-datepicker';
+
+if (document.querySelector('[data-datepicker]')) {
+	const picker = datepicker('[data-datepicker]', {
+		customDays: ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"],
+		customMonths: ["Янв", "Фев", "Мар", "Апр", "Май", "Июн", "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек"],
+		overlayButton: 'Применить',
+		overlayPlaceholder: 'Год (4 цифры)',
+		startDay: 1,
+		formatter: (input, date, instance) => {
+			const value = date.toLocaleDateString()
+			let reDot = /\./g;
+			input.value = value.replace(reDot, "/");
+		},
+		onSelect: function (input, instance, date) {
+
+		}
+	});
+	flsModules.datepicker = picker;
+}
+flsModules.datepickerPopup = pickerPopup;
---
+++ fitness-traiiner/src/js/files/script.js
@@ -0,0 +1,4 @@
+// Подключение функционала "Чертогов Фрилансера"
+import { isMobile, parallaxBg } from "./functions.js";
+// Подключение списка активных модулей
+import { flsModules } from "./modules.js";
