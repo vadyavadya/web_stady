fb141f1bd758326181c1fb5d78d1c897915985cd
Vadim Sergeev <Vadim.Sergeev@evraz.com>
2022-11-09 17:13:13 +0700

    made header and startetd firs block

--- fitness-traiiner/src/js/app.js
+++ fitness-traiiner/src/js/app.js
@@ -75,12 +75,12 @@ import * as flsForms from "./files/forms/forms.js";
 
 /* Работа с полями формы */
 /* Документация: https://template.fls.guru/template-docs/rabota-s-formami.html */
-/* 
+
 flsForms.formFieldsInit({
 	viewPass: false,
 	autoHeight: false
 });
- */
+
 
 /* Oтправка формы */
 /* Документация: https://template.fls.guru/template-docs/rabota-s-formami.html */
@@ -190,7 +190,7 @@ import * as flsScroll from "./files/scroll/scroll.js";
 
 /* Динамический адаптив */
 // Документация: https://template.fls.guru/template-docs/dinamicheskij-adaptiv.html
-// import "./libs/dynamic_adapt.js";
+import "./libs/dynamic_adapt.js";
 
 /* Форматирование чисел */
 // import './libs/wNumb.min.js';
--- fitness-traiiner/src/js/files/script.js
+++ fitness-traiiner/src/js/files/script.js
@@ -2,3 +2,6 @@
 import { isMobile, parallaxBg } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+
+
+
