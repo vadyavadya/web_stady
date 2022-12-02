73e3dcc2093be2c45bee6a85c62f48f754afe1db
Vadim Sergeev <Vadim.Sergeev@evraz.com>
2022-12-02 16:50:25 +0700

    js lern create file desc_js

---
+++ lesson_20/source/Lessons_20/script.js
@@ -0,0 +1,199 @@
+// Проверка мобильного браузера
+// import { isMobile } from "./functions";
+
+/*
+// Пусть переменная "num" будет!
+let num;
+console.log(typeof num);
+console.log(num);
+
+// Пусть переменная "num" содержит число 10
+num = 10;
+console.log(typeof num);
+console.log(num);
+
+// Пусть переменная "num" содержит строку Привет!
+num = "Привет!";
+console.log(typeof num);
+console.log(num);
+*/
+
+// Пусть константа "mainPage" ВСЕГДА содержит объект с классом .page
+// const mainPage = document.querySelector('.page');
+
+/*
+const user = {
+	name: "Максим",
+	age: "26"
+}
+user.name = "Лоцы";
+console.log(user.name);
+*/
+
+// Операторы
+// - + * / 
+// оператор сложения "+" НЕ преобразовывает тип данных
+/*
+let num = 100;
+let sayHi = "Привет";
+let blockHeight = num + sayHi;
+console.log(typeof blockHeight);
+console.log(blockHeight);
+*/
+/*
+let partOne = "Привет,";
+let partTwo = "Вася";
+let sayHi = partOne + partTwo;
+console.log(sayHi);
+*/
+
+// Операторы сравнения > < >= <= == === != !==
+/*
+let numOne = 10;
+let numTwo = "10";
+let result = numOne !== numTwo;
+console.log(typeof result);
+console.log(result);
+*/
+
+// Условное ветвление
+/*
+let numOne = 10;
+let numTwo = 30;
+
+if (numOne > numTwo) {
+	// Выполняем програму,
+	// если условие выполненно (true)
+	console.log('numOne больше чем numTwo');
+} else if (numTwo === 20) {
+	console.log('numTwo равно 20');
+} else {
+	console.log('Условия НЕ выполненны!');
+}
+*/
+
+/*
+let numOne;
+// Возвращают false: undefined, 0, "", NaN
+if (numOne) {
+	console.log('numOne возвращает true');
+} else {
+	console.log('numOne возвращает FALSE');
+}*/
+/*
+const mainPage = document.querySelector('.page');
+if (mainPage) {
+	console.log(mainPage);
+}
+*/
+
+// Логические операторы || && !
+/*
+let numOne = 50;
+let numTwo = 30;
+if (numOne < numTwo && numOne === 50 || numTwo !== 40 && numOne > 35) {
+	console.log('Выполняем код...');
+}
+*/
+/*
+const blockElement = document.querySelector('.block');
+if (!blockElement.classList.contains('active')) {
+	blockElement.classList.add('active');
+}
+*/
+
+//let someVar = "Я строка!";
+//let someVar = 'Я строка!';
+// Обратные кавычки
+//let someVar = `Я строка!`;
+/*
+let someNum = 21;
+let someString = `Мне ${someNum} лет`;
+console.log(someString);
+*/
+
+/*
+// Область видимости
+let someNum = 10;
+let someString;
+if (10 > 5) {
+	someString = "Привет!";
+}
+console.log(someString);
+*/
+
+// Функции
+
+// Создание функции
+/*
+function setConsole(someData) {
+	// Выполняемый код
+	console.log(someData);
+}
+// Вызов функции
+setConsole("Привет!");
+*/
+
+/*
+function getSumm(numOne = 5, numTwo = 10) {
+	return numOne + numTwo;
+}
+
+let someNumber = 580;
+
+if (someNumber === 100) {
+	console.log(getSumm(80, 50));
+} else {
+	console.log(getSumm(10, 10));
+}
+*/
+// ... продолжаем...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lesson_21/source/Lessons_21/script.js
@@ -0,0 +1,232 @@
+// Проверка мобильного браузера
+// import { isMobile } from "./functions";
+
+/*
+let someArray = [];
+someArray.push("number 1");
+someArray.push("number 2");
+
+console.log(someArray);
+
+console.log(someArray.length);
+
+console.log(someArray[0]);
+*/
+
+/*
+// DOM
+// Получаем коллекцию объектов
+const listItems = document.querySelectorAll('.list__item');
+listItems.forEach(function (listItem) {
+	// Работаем с каждым объектом
+});
+// Проверяем
+if (listItems.length) {
+	console.log('Есть!');
+}
+
+// Получаем первый объект
+const list = document.querySelector('.list');
+// Проверяем
+if (list) {
+
+}
+*/
+
+// Работа с классами
+// const listElement = document.querySelector('.list');
+
+//listElement.classList.add('active');  // НЕ СТАВИТЬ ТОЧКУ!!!!!
+//listElement.classList.remove('active');
+//listElement.classList.toggle('active');
+
+/*
+if (listElement && listElement.classList.contains('list')) { // НЕ СТАВИТЬ ТОЧКУ!!!!!
+	console.log('Юху!');
+}
+*/
+/*
+const listItems = document.querySelectorAll('.list__item');
+if (listItems.length) {
+	listItems.forEach(function (listItem) {
+		if (listItem.classList.contains('active')) { // НЕ СТАВИТЬ ТОЧКУ!!!!!
+			console.log('Юху!');
+		}
+	});
+}
+*/
+
+/*
+const someButton = document.querySelector('button');
+// Добавить прослушивание события
+if (someButton) {
+	let someNum = 0;
+	someButton.addEventListener("click", function (e) {
+		++someNum; //someNum = someNum + 1;
+		if (someNum === 3) {
+			console.log("Красавчик!");
+		}
+		console.log("Юху!");
+	});
+}
+*/
+
+/*
+Логика №1
+1) Получаем объект бугрер
+2) Получаем объект меню которое кужно открыть
+3) Вешаем обработчик события click на бургер (п.1)
+4) Добавляем класс к бургеру (п.1)
+5) Добавляем класс к меню (п.2)
+*/
+/*
+const menuBurger = document.querySelector('.some-menu__icon');
+const menuBody = document.querySelector('.some-menu__body');
+
+if (menuBurger) {
+	menuBurger.addEventListener("click", function (e) {
+		menuBurger.classList.toggle('_active');
+		if (menuBody) {
+			menuBody.classList.toggle('_active');
+		}
+	});
+}
+*/
+/*
+Логика №2
+1) Получаем объект бугрер
+2) Получаем объект меню (родительский)
+3) Вешаем обработчик события click на бургер (п.1)
+4) Добавляем класс объекту меню (п.2)
+*/
+/*
+const menuBurger = document.querySelector('.some-menu__icon');
+// Получаем родительсний элемент menuBurger с классом .some-menu
+const menu = menuBurger.closest('.some-menu');
+
+if (menuBurger) {
+	menuBurger.addEventListener("click", function (e) {
+		if (menu) {
+			menu.classList.toggle('_menu-active');
+		}
+	});
+}*/
+
+/*
+// Получаем коллекцию всех объектов с классом .list
+const listItems = document.querySelectorAll('.list');
+// Проверяем есть ли объекты
+if (listItems.length) {
+	// Бегаем по каждому из них
+	// listItem "роздается" в функции, в неё передается
+	// каждый из перебираемых объектов
+	listItems.forEach(function (listItem) {
+		// Если у объекта есть предок с класом .block ...
+		if (listItem.closest('.block')) {
+			// ...добавляем класс _active
+			listItem.classList.add('_active');
+		}
+	});
+}
+*/
+/*
+const listItems = document.querySelectorAll('.list__link');
+if (listItems.length) {
+	listItems.forEach(listItem => {
+		listItem.addEventListener("click", function (e) {
+			listItem.classList.toggle('_active');
+			// Отмена действия по умолчанию
+			e.preventDefault();
+		});
+	});
+}
+*/
+
+
+/*
+Задача:
+1) При клике на ссылку, добавить\убрать класс
+2) При клике не на ссылку удалить все классы с объектов .list__link
+*/
+
+/*
+// Делегирование событий
+// Слушаем клик на всем документе
+const listLinks = document.querySelectorAll('.list__link');
+document.addEventListener("click", function (e) {
+	// Получаем объект на который кликнули
+	const targetElement = e.target;
+	// Проверяем не наш ли это объект?
+	// (ищем класс .list__link среди предков или у самого себя )
+	if (targetElement.closest('.list__link')) {
+		targetElement.closest('.list__link').classList.toggle('_open');
+		e.preventDefault();
+	} else {
+		removeClasses(listLinks, '_open');
+	}
+	if (targetElement.closest('.someButton')) {
+		removeClasses(listLinks, '_open');
+	}
+});
+
+function removeClasses(elements, className) {
+	elements.forEach(function (element) {
+		element.classList.remove(className);
+	});
+}*/
+
+
+/*
+const form = document.querySelector('.form');
+if (form) {
+	form.addEventListener("submit", function (e) {
+		const formNumOne = form.querySelector('#num_1').value;
+		const formNumTwo = form.querySelector('#num_2').value;
+		const formResult = form.querySelector('.form__result');
+
+		formResult.innerHTML = getSumm(formNumOne, formNumTwo);
+
+		e.preventDefault();
+	});
+}
+
+const someButton = document.querySelector('.someButton');
+if (someButton) {
+	someButton.addEventListener("click", function (e) {
+		console.log(getSumm(35, 10));
+
+	});
+}
+function getSumm(numOne, numTwo) {
+	return Number(numOne) + Number(numTwo);
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
